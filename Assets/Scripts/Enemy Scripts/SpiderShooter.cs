327a3d00c635b47d2bedd4bbb6823a0d13ab6cd5
ArcaanST <92755916+ArcaanST@users.noreply.github.com>
2023-01-06 18:56:53 -0300

    Submit Project

---
+++ Assets/Scripts/Enemy Scripts/SpiderShooter.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderShooter : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject bullet;
+
+    private Transform bulletSpawnPoint;
+
+    [SerializeField]
+    private float minShootTime = 2f, maxShootTime = 5f;
+
+    private float shootTimer;
+
+    private void Awake()
+    {
+        bulletSpawnPoint = transform.GetChild(0).transform;
+    }
+
+    private void Start()
+    {
+        //Invoke("ShootBullet", Random.Range(minShootTime, maxShootTime));
+        //shootTimer = Time.time + Random.Range(minShootTime, maxShootTime);
+        StartCoroutine(StartShoting());
+    }
+
+    private void Update()
+    {
+        //if (Time.time > shootTimer)
+        //{
+            //ShootBullet();
+            //shootTimer = Time.time + Random.Range(minShootTime, maxShootTime);
+        //}
+    }
+
+    void ShootBullet()
+    {
+        Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+        //Invoke("ShootBullet", Random.Range(minShootTime, maxShootTime));
+    }
+
+    IEnumerator StartShoting()
+    {
+        yield return new WaitForSeconds(Random.Range(minShootTime, maxShootTime));
+        ShootBullet();
+
+        StartCoroutine(StartShoting());
+
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Level Scripts/Key.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+
+    public delegate void KeyCollected();
+    public static event KeyCollected keyCollectedInfo;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag(TagManager.PLAYER_TAG))
+        {
+            // unlock gate
+            //Lock.instance.UnlockDoor();
+
+            if (keyCollectedInfo != null)
+                keyCollectedInfo();
+
+            Destroy(gameObject);
+        }
+
+    }
+
+
+} // class
